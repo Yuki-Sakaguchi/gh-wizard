595b29a43f934fbde3e8857b652074269ab9a944
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2025-08-10 23:22:46 +0900

    GitHubクライアントの実装

---
+++ internal/utils/gh.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+// CheckGHInstalled は GitHub CLI がインストールされているかどうかをチェックする
+func CheckGHInstalled() error {
+	_, err := exec.LookPath("gh")
+	if err != nil {
+		return fmt.Errorf("GitHub CLI がインストールされていません。https://cli.github.com/ からインストールしてください。")
+	}
+	return nil
+}
+
+// GitGHVersion は GitHub CLI のバージョンを取得する
+func GitGHVersion() (string, error) {
+	cmd := exec.Command("gh", "--version")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("GitHub CLI のバージョンの取得に失敗しました: %w", err)
+	}
+
+	lines := strings.Split(string(output), "\n")
+	if len(lines) > 0 {
+		return strings.TrimSpace(lines[0]), nil
+	}
+
+	return "不明", nil
+}
+
+// CheckGHVersion は GitHub CLI のバージョンが要件を満たすかどうかチェックする
+func CheckGHVersion() error {
+	version, err := GitGHVersion()
+	if err != nil {
+		return err
+	}
+
+	if !strings.Contains(version, "gh version") {
+		return fmt.Errorf("GitHub CLI のバージョン形式が不正です: %s", version)
+	}
+
+	return nil
+}
